b3ab418858574d27fbef89deeda7384797fc61b4
faewd <faew.dev@gmail.com>
2025-08-25 11:41:30 +0100

    Remove elasticsearch

--- src/lib/api/elasticsearch.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from "@elastic/elasticsearch";
-
-export const client = new Client({
-  node: "http://localhost:9200",
-  auth: {
-    username: "elastic",
-    password: "elastic",
-  },
-});

446a327134598dbc996bf62da4347a9b34702a08
faewd <faew.dev@gmail.com>
2025-09-01 18:37:22 +0100

    tweak callback url construction

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -3,7 +3,9 @@ import { auth0 } from "$lib/api/auth";
 import { env } from "$env/dynamic/private";
 
 export const GET: RequestHandler = async (event) => {
-  await auth0.completeInteractiveLogin(new URL(event.url, env.BASE_URL), {
+  const url = new URL(event.url.pathname, env.BASE_URL);
+  url.search = event.url.search;
+  await auth0.completeInteractiveLogin(url, {
     cookies: event.cookies,
   });
   return redirect(302, "/");

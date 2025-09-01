76876d5d0a2579305c960ccd76629d956c0b3a71
faewd <faew.dev@gmail.com>
2025-09-01 20:05:56 +0100

    tweak ArticleView styling

--- src/lib/components/ArticleView.svelte
+++ src/lib/components/ArticleView.svelte
@@ -32,7 +32,7 @@
   {#if editable}
     <a
       href="/edit/{article.title}"
-      class="not-prose absolute top-14 left-2 cursor-pointer rounded bg-amber-950 p-1 text-amber-200 transition-colors hover:bg-amber-900 hover:text-amber-100"
+      class="not-prose static top-14 left-2 cursor-pointer rounded bg-amber-950 p-1 text-amber-200 transition-colors hover:bg-amber-900 hover:text-amber-100"
     >
       <Pencil />
     </a>
@@ -41,7 +41,7 @@
   <section>
     {#if article.metadata.infobox}
       <aside
-        class="not-prose float-right clear-both ml-4 grid w-72 grid-cols-[repeat(2,auto)] gap-2 rounded bg-zinc-950 p-2"
+        class="not-prose float-right clear-both mt-2 ml-4 grid w-72 grid-cols-[repeat(2,auto)] gap-2 rounded bg-zinc-950 p-2"
       >
         <h2
           class="col-span-2 rounded bg-zinc-900 px-4 py-1 text-center font-heading text-xl font-bold text-ice-300"

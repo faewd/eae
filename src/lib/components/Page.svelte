9bf8b123704a6999969182d6283198c604557d7d
faewd <faew.dev@gmail.com>
2025-09-05 08:42:18 +0100

    Improve error page styling on small screens

--- src/lib/components/Page.svelte
+++ src/lib/components/Page.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
+  import cx from "$lib/utils/cx";
+  import type { ClassValue } from "clsx";
   import type { Snippet } from "svelte";
 
   interface Props {
+    class?: ClassValue;
     children: Snippet;
   }
 
-  let { children }: Props = $props();
+  let { children, class: className }: Props = $props();
 </script>
 
-<main class="absolute flex h-dvh w-full justify-center overflow-y-auto pb-12">
+<main class={cx("absolute flex h-dvh w-full justify-center overflow-y-auto pb-12", className)}>
   <div class="max-w-[75ch] flex-grow p-4 md:p-0 lg:mt-10">
     {@render children()}
   </div>
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,28 +2,30 @@
   import { page } from "$app/state";
   import CreateArticleButton from "$lib/components/CreateArticleButton.svelte";
   import SearchBar from "$lib/components/SearchBar.svelte";
+  import Page from "$lib/components/Page.svelte";
   import type { PageProps } from "./$types";
 
   let { data }: PageProps = $props();
 </script>
 
-<main class="flex justify-center">
-  <div class="max-w-[75ch] pt-10 text-center">
-    <h1 class="font-heading text-4xl font-bold text-rose-300">Error - {page.status}</h1>
-    {#if page.error}
-      <p class="text-xl text-rose-100 italic">{page.error?.message}</p>
+<Page class="text-center">
+  <h1 class="font-heading text-4xl font-bold text-rose-300">Error - {page.status}</h1>
+  {#if page.error}
+    <p class="text-xl text-rose-100 italic">{page.error?.message}</p>
 
-      {#if page.status === 404}
-        <p class="mt-10 mb-2 text-zinc-400">
-          Can't find what you're looking for? Try searching for it:
-        </p>
-        <SearchBar />
+    {#if page.status === 404}
+      <p class="mt-10 mb-2 text-zinc-400">
+        Can't find what you're looking for? Try searching for it:
+      </p>
 
-        {#if page.url.pathname.startsWith("/wiki/") && data.user?.isAdmin}
-          <p class="mt-8 mb-4">Or, create the article now:</p>
-          <CreateArticleButton title={decodeURIComponent(page.url.pathname.substring(6))} />
-        {/if}
+      <div class="flex w-full justify-center">
+        <SearchBar class="max-w-80" />
+      </div>
+
+      {#if page.url.pathname.startsWith("/wiki/") && data.user?.isAdmin}
+        <p class="mt-8 mb-4">Or, create the article now:</p>
+        <CreateArticleButton title={decodeURIComponent(page.url.pathname.substring(6))} />
       {/if}
     {/if}
-  </div>
-</main>
+  {/if}
+</Page>
